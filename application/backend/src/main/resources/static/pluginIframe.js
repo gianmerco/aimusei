ece3925d5bee85a8cbb40060fcb72357f60acc3f
Antonio Palamara <a.palamara@prismaprogetti.it>
2025-10-16 12:04:03 +0200

    add url public into pluginIframe: 'https://api-coll.museiitaliani.it/aimusei/'

--- application/backend/src/main/resources/static/pluginIframe.js
+++ application/backend/src/main/resources/static/pluginIframe.js
@@ -2,12 +2,8 @@
 
   function IframePlugin(options) {
     this.options = Object.assign({
-      //local config
-      pathIcon: './lib/plugin',   // icon default path 
-      urlIframe: 'http://localhost:4200'
-      //cluster config
-      //pathIcon: 'http://aimusei.local/aimusei/api',   // icon default path 
-      //urlIframe: 'http://aimusei.local/aimusei/',
+	pathIcon: 'https://api-coll.museiitaliani.it/aimusei/api'
+    urlIframe: 'https://api-coll.museiitaliani.it/aimusei/',
       observerDom: false
     }, options || {});
 
