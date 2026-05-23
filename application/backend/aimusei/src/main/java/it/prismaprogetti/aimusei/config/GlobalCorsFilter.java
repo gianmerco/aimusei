09c4d421914038871cfda2c8cfc2cd77fc4fa649
Samuele Maritato <s.maritato1999@gmail.com>
2026-05-23 20:25:59 +0200

    fix: aggiunte origins coll (portale-coll/smn-coll/api-coll/akamaicdn-coll) in GlobalCorsFilter
    Le origin -coll erano state rimosse il 21 Maggio (commit 28915e5) preparando il rilascio prod. Pipeline ora builda da versione-1.2 quindi backend coll rifiutava preflight CORS dal portale-coll causando 'No Access-Control-Allow-Origin header' su POST generateImage. Le origin -coll risolvono solo dalla rete coll, niente rischio cross-env.

--- application/backend/aimusei/src/main/java/it/prismaprogetti/aimusei/config/GlobalCorsFilter.java
+++ application/backend/aimusei/src/main/java/it/prismaprogetti/aimusei/config/GlobalCorsFilter.java
@@ -26,11 +26,18 @@ import jakarta.servlet.http.HttpServletResponse;
 public class GlobalCorsFilter implements Filter {
 
     private static final Set<String> ALLOWED_ORIGINS = Set.of(
+        // PROD origins
         "https://akamaicdn.museiitaliani.it",
         "https://portale.museiitaliani.it",
         "https://smn.museiitaliani.it",
         "https://museiitaliani.it",
         "https://sistemamusealenazionale.beniculturali.it",
+        // COLLAUDO origins (i nomi -coll risolvono solo su rete coll; nessun leak prod)
+        "https://akamaicdn-coll.museiitaliani.it",
+        "https://portale-coll.museiitaliani.it",
+        "https://smn-coll.museiitaliani.it",
+        "https://api-coll.museiitaliani.it",
+        // local dev
         "http://localhost:5173",
         "http://localhost:8000",
         "http://localhost:8080",
